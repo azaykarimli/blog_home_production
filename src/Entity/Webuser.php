0f71ff579131da1e6706e98aec4b951317205939
azaykarimli <azaykerimli@hotmail.com>
2022-05-31 02:11:34 +0200

    auth and web server configured

--- src/Entity/Webuser.php
+++ src/Entity/Webuser.php
@@ -4,10 +4,12 @@ namespace App\Entity;
 
 use App\Repository\WebuserRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: WebuserRepository::class)]
+#[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
 class Webuser implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
